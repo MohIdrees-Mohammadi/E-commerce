48d2bf2648d927b66340c915b6401593ea575df8
Idrees <mohidrees.mohammadi786@gmail.com>
2025-10-29 14:46:53 +0430

    Hero Section has been created

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
-
+import heroBanner from '../images/hero-image.jpg'
+import { FaArrowRight } from "react-icons/fa6";
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="relative h-[600px] overflow-hidden">
+        <div className="absolute">
+          <img
+            src={heroBanner}
+            alt="Heritage Fashion"
+            className="w-full h-full object-cover"
+          />
+          <div className="absolute inset-0  bg-gradient-to-r from-red-900/80 to-red-100/20" />
+         
+        </div>
+
+        <div className='relative  h-full w-3xl flex flex-col justify-center items-center'>
+          
+          <div className='w-[600px] ml-100 '>
+            <h1 className='text-6xl text-white  font-bold font-serif'>Celebrate Heritage Through Fashion</h1>
+            <p className='text-2xl mt-7 text-gray-100'>Discover authentic national dresses and cultural attire that blend tradition with modern elegance.</p>
+            <div className='mt-5 flex'>
+              <button className='bg-yellow-500 cursor-pointer flex px-4 py-2 rounded items-center gap-3 font-semibold'>
+                Shop Collection
+                <FaArrowRight  />
+              </button>
+
+              <button className='bg-white cursor-pointer flex px-4 py-2 rounded items-center gap-3 ml-3 font-semibold'>Our Story</button>
+            </div>
+          </div>
+        </div>
+        
+      </section>
+
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,36 +7,37 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet"
-import { MdOutlineShoppingCart } from "react-icons/md";
+import { CiShoppingCart } from "react-icons/ci";
 import { FiUserPlus } from "react-icons/fi";
 import { IoLogOutOutline } from "react-icons/io5";
 import { BsCartFill } from "react-icons/bs";
 import { NavLink } from 'react-router-dom';
+import { FiShoppingBag } from "react-icons/fi";
 
 const Header = () => {
-  const [isEmpty, setIsEmpty] = React.useState(false);
+  const [isEmpty, setIsEmpty] = React.useState(true);
 
   return (
-    <header className="bg-gray-50 flex align items-center justify-between px-[120px] py-5">
-      <h1>Heritage Threads</h1>
+    <header className="bg-red-200/30 flex align items-center justify-between px-[120px] py-5">
+      <h1 className='font-serif text-2xl font-bold'>Heritage Threads</h1>
 
-      <nav className='flex gap-2'>
-        <NavLink className={({ isActive}) =>
+      <nav className='flex gap-5'>
+        <NavLink className={({ isActive }) =>
           isActive ? "font-semibold text-gray-900" : "text-gray-900"
         } to="/">Home</NavLink>
 
 
-        <NavLink className={({ isActive}) =>
+        <NavLink className={({ isActive }) =>
           isActive ? "font-semibold text-gray-900" : "text-gray-900"
         } to="/about-us">About</NavLink>
 
 
-        <NavLink className={({ isActive}) =>
+        <NavLink className={({ isActive }) =>
           isActive ? "font-semibold text-gray-900" : "text-gray-900"
         } to="/shop">Shop</NavLink>
 
 
-        <NavLink className={({ isActive}) =>
+        <NavLink className={({ isActive }) =>
           isActive ? "font-semibold text-gray-900" : "text-gray-900"
         } to="/contact">Contact</NavLink>
       </nav>
@@ -45,9 +46,9 @@ const Header = () => {
       <div className='flex gap-4'>
         <Sheet>
           <SheetTrigger>
-            <div className="p-2 hover:bg-gray-200 rounded cursor-pointer transition">
-      <MdOutlineShoppingCart size={25} />
-    </div>
+            <div className="p-2 hover:bg-red-200 rounded cursor-pointer transition">
+              <CiShoppingCart  size={25} />
+            </div>
           </SheetTrigger>
           <SheetContent>
             <SheetHeader>
@@ -55,7 +56,7 @@ const Header = () => {
               <SheetDescription>
                 {isEmpty ? (
                   <div className="flex flex-col gap-4 items-center justify-center h-[90vh]">
-                    <BsCartFill size={70} color="red" />
+                    <FiShoppingBag size={70} className='text-red-700/60'/>
                     <h1 className="text-4xl">Your Cart is Empty</h1>
                     <p>Discover our beautiful collection of traditional attire</p>
                     <button className="px-7 py-2 text-white rounded shadow bg-red-500">
@@ -71,7 +72,20 @@ const Header = () => {
         </Sheet>
 
         <div className='flex gap-2 items-center'>
-          <button className='px-5 py-2 bg-green-400 rounded'>Sign in</button>
+          <button className='px-3 py-2 bg-red-900/10 border border-red-100 font-bold text-red-900 rounded-md'>
+            <div className='flex gap-3 items-center'>
+              <IoLogOutOutline size={20} />
+              Sign in
+            </div>
+
+          </button>
+          <button className='px-3 py-2 bg-red-900 font-bold text-white rounded-md'>
+            <div className='flex gap-3 items-center'>
+              <FiUserPlus size={20} />
+              Sign up
+            </div>
+
+          </button>
         </div>
       </div>
 
