9678aea8483c305e6b3b51794a1e1d4cfbf124e3
Khwaja Sediqullah Fazly <fazlysediqullah2000@gmail.com>
2025-11-03 21:04:24 +0430

    About has benn Completed and Home has been updated

--- src/components/TrustedPartenars.jsx
+++ src/components/TrustedPartenars.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const TrustedPartenars = ({ partenars }) => {
   return (
-    <div className="flex flex-wrap justify-center gap-10 m-10">
+    <div className="flex flex-wrap justify-center gap-10 m-10 ">
       {partenars.map((partenar) => (
         <div
           key={partenar.id}
-          className="bg-[#FFFFFF] border p-6 w-[280px] h-[190px] rounded-2xl text-center shadow-md hover:shadow-lg transition-shadow">
-          <h1 className="bg-[#b02d4e] rounded-full text-center mx-auto mb-2 pt-6 w-20 h-20 text-[24px] font-family-serif text-[#FBF3E9] font-bold hover:scale-105 shadow-2xl">
+          className="bg-[#FFFFFF] border p-6 w-[280px] h-[190px] rounded-2xl text-center shadow-md hover:shadow-lg transition-shadow hover:scale-105">
+          <h1 className="bg-[#b02d4e] rounded-full text-center mx-auto mb-2 pt-6 w-20 h-20 text-[24px] font-family-serif text-[#FBF3E9] font-bold shadow-2xl">
             {partenar.logo}
           </h1>
           <h1 className="text-[16px] mb-1 font-family-serif text-[#450817] font-bold">
